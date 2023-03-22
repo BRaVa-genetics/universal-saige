bb4d82637d9fac30fd8e57a0e52056ef90c04832
astheeggeggs <duncan.stuart.palmer@gmail.com>
2023-03-22 13:21:52 +0000

    some progress creating universal-saige bash scripts

--- check.sh
+++ check.sh
@@ -1,36 +1,35 @@
-check_exome_data(bimFile, bedFile, famFile, vcfFile){
-    if [[ $bimFile == "" && $bedFile == "" && $famFile == "" && $vcfFile == "" ]];
-    then
-        echo "Error: either plink files or VCF file is required"
-        exit(1)
-    if [[ $bimFile != "" && $bedFile != "" && $famFile != "" && $vcfFile != "" ]];
-    then
-        echo "Error: either plink files or VCF file is required"
-        exit(1)
+#!/bin/bash
+
+# check_exome_data {
+#   if [[ $bimFile == "" && $bedFile == "" && $famFile == "" && $vcfFile == "" ]]; then
+#     echo "Error: either plink files or VCF file is required"
+#     exit(1)
+#   if [[ $bimFile != "" && $bedFile != "" && $famFile != "" && $vcfFile != "" ]]; then
+#     echo "Error: either plink files or VCF file is required"
+#     exit(1)
+#   fi
+# }
+
+function check_container_env() {
+  # Check if singularity or docker is installed
+  SINGULARITY="$1"
+  if [[ ${SINGULARITY} = true ]]; then
+    if [[ $(which singularity) == "" ]]; then
+      echo "Error: singularity is not installed"
+      exit 1
     fi
+  else
+    if [[ $(which docker) == "" ]]; then
+        echo "Error: docker is not installed"
+        exit 1
+    fi
+  fi
+  echo "singularity or docker found"
 }
 
-check_container_env(is_singularity){
-    # check if singularity or docker is installed:
-    if [ is_singularity ]
-    then
-        if [ system("which singularity") != 0 ]
-        then
-            echo "Error: singularity is not installed"
-            exit(1)
-        fi
-    elif [ !is_singularity ]
-        if [system("which docker") != 0]
-        then
-            echo "Error: docker is not installed"
-            exit(1)
-        fi
-    }
-}
-
-check_sparse_grm_data(genotypeFileBim, genotypeFileBed, genotypeFileFam, exomeFileBim, exomeFileBed, exomeFileFam){
-    if [[ $genotypeFileBim == "" && $genotypeFileBed == "" && $genotypeFileFam == "" && $exomeFileBim == "" && $exomeFileBed == "" && $exomeFileFam == "" ]];
-        echo "Error: either genotype plink files or exome plink files are required for sparse GRM"
-        exit(1)
-    }
-}
+# check_sparse_grm_data {
+#     if [[ $genotypeFileBim == "" && $genotypeFileBed == "" && $genotypeFileFam == "" && $exomeFileBim == "" && $exomeFileBed == "" && $exomeFileFam == "" ]];
+#         echo "Error: either genotype plink files or exome plink files are required for sparse GRM
+#         exit(1)
+#     }
+# }
