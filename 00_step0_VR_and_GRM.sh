7aae696f78684812b4d40093452dffd7f26b2e76
Barney Hill <barneyhill1@gmail.com>
2023-04-06 14:02:43 +0100

    Shifted VR + GRM creation to 00

--- 00_step0_VR_and_GRM.sh
+++ 00_step0_VR_and_GRM.sh
@@ -56,20 +56,81 @@ generate_GRM(){
   else
     echo "Operating system not compatible with the code"
   fi
+
   echo $SAMPLEIDS
-  ./resources/plink \
-    --bfile "${GENOTYPE_PLINK}" \
-    --keep-fam ${SAMPLEIDS} \
-    --indep-pairwise 50 5 0.05 \
-    --out "${OUT}"
+
+  if [[ $GENETIC_DATA_FORMAT == "vcf" || $GENETIC_DATA_FORMAT == "bgen" ]]; then
+    # get list of files with format in dir:
+    FILES=$(ls ${GENETIC_DATA_DIR}/*${GENETIC_DATA_FORMAT}*)
+    # iterate files (bash):
+    if [[ $GENETIC_DATA_FORMAT == "vcf" ]]; then
+        for FILE in $FILES
+        do
+            echo "Processing $f file..."
+            # take action on each file. $f store current file name
+            ./resources/plink \
+                --vcf ${GENETIC_DATA_DIR}/${FILE} \
+                --keep-fam ${SAMPLEIDS} \
+                --indep-pairwise 50 5 0.05 \
+                --out /tmp/${FILE}
+        done
+        for FILE in $FILES
+        do
+            ./resources/plink \
+                --vcf "${GENOTYPE_PLINK}" \
+                --keep-fam ${SAMPLEIDS} \
+                --extract "${OUT}.prune.in" \
+                --make-bed \
+                --out "${OUT}"
+            
+        done
+    elif [[ $GENETIC_DATA_FORMAT == "bgen" ]]; then
+        for FILE in $FILES
+        do
+            echo "Processing $f file..."
+            # take action on each file. $f store current file name
+            ./resources/plink \
+                --bgen ${GENETIC_DATA_DIR}/${FILE} \
+                --keep-fam ${SAMPLEIDS} \
+                --indep-pairwise 50 5 0.05 \
+                --out /tmp/${FILE}
+        done
+        for FILE in $FILES
+        do
+            ./resources/plink \
+                --bgen "${GENOTYPE_PLINK}" \
+                --keep-fam ${SAMPLEIDS} \
+                --extract /tmp/${FILE}.prune.in \
+                --make-bed \
+                --out "${OUT}"
+        done
+    fi
+  elif [[ $GENETIC_DATA_FORMAT == "plink" ]]; then
+    FILES=$(ls ${GENETIC_DATA_DIR}/*.bed)
+
+    for FILE in $FILES
+    do
+        echo "Processing $f file..."
+        # take action on each file. $f store current file name
+        ./resources/plink \
+            --plink ${GENETIC_DATA_DIR}/${FILE%.bed} \
+            --keep-fam ${SAMPLEIDS} \
+            --indep-pairwise 50 5 0.05 \
+            --out /tmp/${FILE}
+    done
+    for FILE in $FILES
+    do
+        ./resources/plink \
+            --plink ${GENETIC_DATA_DIR}/${FILE%.bed} \
+            --keep-fam ${SAMPLEIDS} \
+            --extract /tmp/${FILE}.prune.in \
+            --make-bed \
+            --out "${OUT}"
+    done
+
+  fi
 
   # Extract set of pruned variants and export to bfile
-  ./resources/plink \
-    --bfile "${GENOTYPE_PLINK}" \
-    --keep-fam ${SAMPLEIDS} \
-    --extract "${OUT}.prune.in" \
-    --make-bed \
-    --out "${OUT}"
   
   cmd="createSparseGRM.R \
     --plinkFile="${HOME}/${OUT}" \
@@ -86,11 +147,8 @@ generate_GRM(){
   SPARSEGRMID="${OUT}.sparseGRM.mtx.sampleIDs.txt"
 
 }
-generate_plink_for_vr_from_exome(){
 
-}
-
-generate_plink_for_vr_from_genotype(){
+generate_plink_for_vr(){
   echo "Generating plink file for vr..."
 
   wget -nc https://s3.amazonaws.com/plink2-assets/plink2_linux_x86_64_20230325.zip -P resources/
@@ -139,47 +197,21 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
-    -t|--traitType)
-      TRAITTYPE="$2"
-      if ! ( [[ ${TRAITTYPE} == "quantitative" ]] || [[ ${TRAITTYPE} == "binary" ]] ); then
-        echo "Trait type is not in {quantitative,binary}"
-        exit 1
-      fi
+    -p|--geneticDataDirectory)
+      GENETIC_DATA_DIR="$2"
       shift # past argument
       shift # past value
       ;;
-    -p|--genotypePlink)
-      GENOTYPE_PLINK="$2"
+    --geneticDataFormat)
+      GENETIC_DATA_FORMAT="$2"
       shift # past argument
       shift # past value
       ;;
-    --sparseGRM)
-      SPARSEGRM="$2"
+    --generate_GRM)
       shift # past argument
       shift # past value
       ;;
-    --sparseGRMID)
-      SPARSEGRMID="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --phenoFile)
-      PHENOFILE="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --phenoCol)
-      PHENOCOL="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    -c|--covarColList)
-      COVARCOLLIST="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --categCovarColList)
-      CATEGCOVARCOLLIST="$2"
+    --generate_plink_for_vr)
       shift # past argument
       shift # past value
       ;;
@@ -188,26 +220,17 @@ while [[ $# -gt 0 ]]; do
       shift
       shift
       ;; 
-    -i|--sampleIDCol)
-      SAMPLEIDCOL="$2"
-      shift # past argument
-      shift # past value
-      ;;
     -h|--help)
-      echo "usage: 01_step1_fitNULLGLMM.sh
+      echo "usage: 00_step0_VR_and_GRM.sh
   required:
-    -t,--traitType: type of the trait {quantitative,binary}.
-    --genotypePlink: plink filename prefix of bim/bed/fam files. These must be present in the working directory at ./in/plink_for_vr_bed/
-    --sparseGRM: filename of the sparseGRM .mtx file. This must be present in the working directory at ./in/sparse_grm/
-    --sparseGRMID: filename of the sparseGRM ID file. This must be present in the working directory at ./in/sparse_grm/
-    --phenoFile: filename of the phenotype file. This must be present in the working directory at ./in/pheno_files/
-    --phenoCol: the column names of the phenotype to be analysed in the file specified in --phenoFile.
+    --geneticDataDirectory: directory containing the genetic data (genotype/WES/WGS data in the format plink/vcf/bgen)
+    --geneticDataFormat: format of the genetic data {plink,vcf,bgen}.
   optional:
     -o,--outputPrefix:  output prefix of the SAIGE step 1 output.
     -s,--isSingularity (default: false): is singularity available? If not, it is assumed that docker is available.
-    -c,--covarColList: comma separated column names (e.g. age,pc1,pc2) of continuous covariates to include as fixed effects in the file specified in --phenoFile.
-    --categCovarColList: comma separated column names of categorical variables to include as fixed effects in the file specified in --phenoFile.
     --sampleIDCol (default: IID): column containing the sample IDs in the phenotype file, which must match the sample IDs in the plink files.
+    --generate_GRM (default: false): generate GRM for the genetic data.
+    --generate_plink_for_vr (default: false): generate plink file for vr.
       "
       shift # past argument
       ;;
@@ -225,8 +248,16 @@ done
 set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
 
 # Checks
-if [[ ${TRAITTYPE} == "" ]]; then
-  echo "traitType not set"
+
+# check if genetic data format in vcf, bgen or plink:
+if [[ ${GENETIC_DATA_FORMAT} != "vcf" ]] && [[ ${GENETIC_DATA_FORMAT} != "bgen" ]] && [[ ${GENETIC_DATA_FORMAT} != "plink" ]]; then
+  echo "geneticDataFormat must be in {vcf,bgen,plink}"
+  exit 1
+fi
+
+# check if genetic data directory exists and if files with the correct extension are present:
+if [[ ! -d ${GENETIC_DATA_DIR} ]]; then
+  echo "geneticDataDirectory does not exist"
   exit 1
 fi
 
@@ -234,62 +265,18 @@ if [[ ${SAMPLEIDS} != "" ]]; then
   SAMPLEIDS=${HOME}/$SAMPLEIDS
 fi
 
-if [[ ${SPARSEGRM} == "" || ${SPARSEGRMID} == "" ]]; then
-  echo "Sparse GRM .mtx file not set. Generating sparse GRM from genotype or exome sequence data."
-  echo "Will attempt to generate GRM"
-fi
-
-if [[ ${PHENOFILE} == "" ]]; then
-  echo "phenoFile not set"
-  exit 1
-fi
-
-if [[ ${PHENOCOL} == "" ]]; then
-  echo "phenoCol not set"
-  exit 1
-fi
-
 if [[ $OUT = "out" ]]; then
   echo "Warning: outputPrefix not set, setting outputPrefix to ${PHENOCOL}. Check that this will not overwrite existing files."
   OUT="${PHENOCOL}"
 fi
 
-if [[ $COVARCOLLIST = "" ]]; then
-  echo "Warning: no continuous fixed effeect covariates included."
-fi
-
-if [[ $CATEGCOVARCOLLIST = "" ]]; then
-  echo "Warning: no categorical fixed effeect covariates included."
-fi
-
 echo "OUT               = ${OUT}"
 echo "SINGULARITY       = ${SINGULARITY}"
-echo "TRAITTYPE         = ${TRAITTYPE}"
 echo "PLINK             = ${PLINK_WES}.{bim/bed/fam}"
-echo "SPARSEGRM         = ${SPARSEGRM}"
-echo "SPARSEGRMID       = ${SPARSEGRMID}"
-echo "PHENOFILE         = ${PHENOFILE}"
-echo "PHENOCOL          = ${PHENOCOL}"
-echo "COVARCOLLIST      = ${COVARCOLLIST}"
-echo "CATEGCOVARCOLLIST = ${CATEGCOVARCOLLIST}"
 echo "SAMPLEIDS         = ${SAMPLEIDS}"
-echo "SAMPLEIDCOL       = ${SAMPLEIDCOL}"
 
 check_container_env $SINGULARITY
  
-if [[ "$PHENOCOL" =~ .*"-".* || "$PHENOCOL" =~ .*",".* || "$PHENOCOL" =~ .*"=".* ]]; then
-  echo "Phenotype name cannot contain \"-\" or \",\" or \"=\""
-  exit 1
-fi
-
-if [[ ${SPARSEGRM} == "" || ${SPARSEGRMID} == "" ]]; then
-  if [[ ${GENOTYPE_PLINK} == "" ]]; then
-    echo "Genotype plink files plink.{bim,bed,fam} not set - cannot generate GRM!"
-    exit 1
-  fi
-  generate_GRM
-fi
-
 if [[ ${SINGULARITY} = true && ! $( test -f "saige-${saige_version}.sif" ) ]]; then
   singularity pull "saige-${saige_version}.sif" "docker://wzhou88/saige:${saige_version}"
 elif [[ ${SINGULARITY} = false ]]; then
@@ -305,32 +292,8 @@ WD=$( pwd )
 # Get number of threads
 n_threads=$(( $(nproc --all) - 1 ))
 
-# Get inverse-normalize flag if trait_type=="quantitative"
-if [[ ${TRAITTYPE} == "quantitative" ]]; then
-  echo "Quantitative trait passed to SAIGE, perform IRNT"
-  INVNORMALISE=TRUE
-else
-  echo "Binary trait passed to SAIGE"
-  INVNORMALISE=FALSE
+if [[ ${GENERATE_GRM} = true ]]; then
+  generate_GRM
+elif [[ ${GENERATE_PLINK_FOR_VR} = true ]]; then
+  generate_plink_for_vr
 fi
-
-cmd="""step1_fitNULLGLMM.R \
-      --plinkFile "${HOME}/${GENOTYPE_PLINK}" \
-	  --sparseGRMFile ${HOME}/${SPARSEGRM} \
-      --sparseGRMSampleIDFile ${HOME}/${SPARSEGRMID} \
-      --useSparseGRMtoFitNULL=TRUE \
-      --phenoFile ${HOME}/${PHENOFILE} \
-      --skipVarianceRatioEstimation FALSE \
-      --traitType=${TRAITTYPE} \
-      --invNormalize=${INVNORMALISE} \
-      --phenoCol ""${PHENOCOL}"" \
-      --covarColList ""${COVARCOLLIST}"" \
-      --qCovarColList=""${CATEGCOVARCOLLIST}"" \
-      --sampleIDColinphenoFile=${SAMPLEIDCOL} \
-      --outputPrefix="${HOME}/${OUT}" \
-      --IsOverwriteVarianceRatioFile=TRUE \
-      --nThreads=${n_threads} \
-      --isCateVarianceRatio=TRUE \
-	  --SampleIDIncludeFile=${SAMPLEIDS}"""
-
-run_container
